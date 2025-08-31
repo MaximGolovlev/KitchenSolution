5f786e2c92cbd398b5bd7ac88e364e054d2dfb75
Maxim Golovlev <golovlev.maxim@gmail.com>
2025-08-31 23:04:36 +0300

    Initial Commit

---
+++ KitchenSolution/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KitchenSolution
+//
+//  Created by Maxim Golovlev on 31.08.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KitchenSolution/KitchenSolutionApp.swift
@@ -0,0 +1,17 @@
+//
+//  KitchenSolutionApp.swift
+//  KitchenSolution
+//
+//  Created by Maxim Golovlev on 31.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct KitchenSolutionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
